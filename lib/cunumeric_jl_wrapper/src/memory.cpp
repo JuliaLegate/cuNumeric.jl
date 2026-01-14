27c026878bb0e2586407cc45a19bd74b5ddb2690
krasow <krasow@u.northwestern.edu>
2026-01-14 13:19:44 -0600

    query interface now does both cpu+gpu

--- lib/cunumeric_jl_wrapper/src/memory.cpp
+++ lib/cunumeric_jl_wrapper/src/memory.cpp
@@ -15,144 +15,107 @@ constexpr uint64_t MiB = KiB * 1024ull;
 constexpr uint64_t GiB = MiB * 1024ull;
 
 using Legion::Machine;
-static uint64_t query_machine_config() {
+
+static inline uint64_t query_machine_config_common(
+    Realm::Processor::Kind proc_kind, Realm::Memory::Kind mem_kind) {
   Machine legion_machine{Machine::get_machine()};
+  uint64_t total_mem = 0;
+
+  Machine::ProcessorQuery procs =
+      Machine::ProcessorQuery(legion_machine).only_kind(proc_kind);
+
+  for (auto pit = procs.begin(); pit != procs.end(); ++pit) {
+    auto proc = *pit;
+    assert(proc.kind() == proc_kind);
+
+    Realm::Machine::MemoryQuery local_memories =
+        Machine::MemoryQuery(legion_machine)
+            .only_kind(mem_kind)
+            .same_address_space_as(proc);
+
+    for (auto mit = local_memories.begin(); mit != local_memories.end();
+         ++mit) {
+      auto mem = *mit;
+      assert(mem.kind() == mem_kind);
+      total_mem += mem.capacity();
+    }
+  }
+
+  return total_mem;
+}
+
+static uint64_t query_machine_config_host() {
+  return query_machine_config_common(Realm::Processor::LOC_PROC,
+                                     Realm::Memory::SYSTEM_MEM);
+}
 
 #if LEGATE_DEFINED(LEGATE_USE_CUDA)
-  Machine::ProcessorQuery gpus = Machine::ProcessorQuery(legion_machine)
-                                     .only_kind(Realm::Processor::TOC_PROC);
-
-  uint64_t total_fb_mem = 0;
-  uint64_t gpus_count = gpus.count();
-
-  // for each GPU
-  for (auto it = gpus.begin(); it != gpus.end(); ++it) {
-    auto proc = *it;
-    assert(proc.kind() == Realm::Processor::TOC_PROC);
-
-    // get all the FB memories local to this GPU
-    Realm::Machine::MemoryQuery local_memories =
-        Machine::MemoryQuery(legion_machine)
-            .only_kind(Realm::Memory::GPU_FB_MEM)
-            .same_address_space_as(proc);
-
-    // TODO: will this ever have multiple GPU_FB_MEM memories???
-    for (auto mem_it = local_memories.begin(); mem_it != local_memories.end();
-         ++mem_it) {
-      auto mem = *mem_it;
-      assert(mem.kind() == Realm::Memory::GPU_FB_MEM);
-
-      total_fb_mem += mem.capacity();
-    }
-  }
-  // std::cout << "Detected " << gpus_count << " GPUs with " << total_fb_mem /
-  // MiB << " MB each, total " << total_fb_mem / GiB << " GB\n";
-  return total_fb_mem;
-#else
-  uint64_t total_system_mem = 0;
-  Machine::ProcessorQuery cpus = Machine::ProcessorQuery(legion_machine)
-                                     .only_kind(Realm::Processor::LOC_PROC);
-
-  for (auto it = cpus.begin(); it != cpus.end(); ++it) {
-    auto proc = *it;
-    assert(proc.kind() == Realm::Processor::LOC_PROC);
-
-    // get all the SYSTEM memories local to this CPU
-    Realm::Machine::MemoryQuery local_memories =
-        Machine::MemoryQuery(legion_machine)
-            .only_kind(Realm::Memory::SYSTEM_MEM)
-            .same_address_space_as(proc);
-
-    // TODO: will this ever have multiple SYSTEM memories???
-    for (auto mem_it = local_memories.begin(); mem_it != local_memories.end();
-         ++mem_it) {
-      auto mem = *mem_it;
-      assert(mem.kind() == Realm::Memory::SYSTEM_MEM);
-
-      total_system_mem += mem.capacity();
-    }
-  }
-  // std::cout << "System memory: " << total_system_mem / GiB << "GB\n";
-  return total_system_mem;
-#endif
+static uint64_t query_machine_config_device() {
+  return query_machine_config_common(Realm::Processor::TOC_PROC,
+                                     Realm::Memory::GPU_FB_MEM);
 }
+#endif
 
 extern "C" {
 
-int nda_recalibrate_allocator() {
-  auto runtime = legate::Runtime::get_runtime();
-  runtime->issue_execution_fence(true);  // block = true
-
+static inline uint64_t query_allocated_bytes_common(
+    Realm::Processor::Kind proc_kind, Realm::Memory::Kind mem_kind) {
   Machine legion_machine{Machine::get_machine()};
   auto legion_runtime = Legion::Runtime::get_runtime();
   auto ctx = Legion::Runtime::get_context();
 
-  // query current allocated bytes
-#if LEGATE_DEFINED(LEGATE_USE_CUDA)
-  Machine::ProcessorQuery gpus = Machine::ProcessorQuery(legion_machine)
-                                     .only_kind(Realm::Processor::TOC_PROC);
-
   uint64_t current_bytes = 0;
 
-  // for each GPU
-  for (auto it = gpus.begin(); it != gpus.end(); ++it) {
-    auto proc = *it;
-    assert(proc.kind() == Realm::Processor::TOC_PROC);
+  Machine::ProcessorQuery procs =
+      Machine::ProcessorQuery(legion_machine).only_kind(proc_kind);
 
-    // get all the FB memories local to this GPU
+  for (auto pit = procs.begin(); pit != procs.end(); ++pit) {
+    auto proc = *pit;
+    assert(proc.kind() == proc_kind);
+
     Realm::Machine::MemoryQuery local_memories =
         Machine::MemoryQuery(legion_machine)
-            .only_kind(Realm::Memory::GPU_FB_MEM)
+            .only_kind(mem_kind)
             .same_address_space_as(proc);
 
-    for (auto mem_it = local_memories.begin(); mem_it != local_memories.end();
-         ++mem_it) {
-      auto mem = *mem_it;
-      assert(mem.kind() == Realm::Memory::GPU_FB_MEM);
+    for (auto mit = local_memories.begin(); mit != local_memories.end();
+         ++mit) {
+      auto mem = *mit;
+      assert(mem.kind() == mem_kind);
 
       size_t available = legion_runtime->query_available_memory(ctx, mem);
       size_t capacity = mem.capacity();
       current_bytes += (capacity - available);
     }
   }
-#else
-  Machine::ProcessorQuery cpus = Machine::ProcessorQuery(legion_machine)
-                                     .only_kind(Realm::Processor::LOC_PROC);
-
-  uint64_t current_bytes = 0;
-
-  // for each CPU
-  for (auto it = cpus.begin(); it != cpus.end(); ++it) {
-    auto proc = *it;
-    assert(proc.kind() == Realm::Processor::LOC_PROC);
-
-    // get all the SYSTEM memories local to this CPU
-    Realm::Machine::MemoryQuery local_memories =
-        Machine::MemoryQuery(legion_machine)
-            .only_kind(Realm::Memory::SYSTEM_MEM)
-            .same_address_space_as(proc);
-
-    for (auto mem_it = local_memories.begin(); mem_it != local_memories.end();
-         ++mem_it) {
-      auto mem = *mem_it;
-      assert(mem.kind() == Realm::Memory::SYSTEM_MEM);
-
-      size_t available = legion_runtime->query_available_memory(ctx, mem);
-      size_t capacity = mem.capacity();
-      current_bytes += (capacity - available);
-    }
-  }
-#endif
 
   return current_bytes;
 }
 
-uint64_t nda_query_device_memory() {
-  uint64_t total = query_machine_config();
-  if (total == 0) {
-    total = 8ull * GiB;
-    std::cerr << "Warning: Unable to query device memory, defaulting to 8 GB\n";
-  }
+uint64_t nda_query_allocated_device_memory() {
+#if LEGATE_DEFINED(LEGATE_USE_CUDA)
+  uint64_t allocated = query_allocated_bytes_common(Realm::Processor::TOC_PROC,
+                                                    Realm::Memory::GPU_FB_MEM);
+#else
+  uint64_t allocated = 0;
+#endif
+  return allocated;
+}
+uint64_t nda_query_allocated_host_memory() {
+  uint64_t allocated = query_allocated_bytes_common(Realm::Processor::LOC_PROC,
+                                                    Realm::Memory::SYSTEM_MEM);
+  return allocated;
+}
+
+uint64_t nda_query_total_device_memory() {
+#if LEGATE_DEFINED(LEGATE_USE_CUDA)
+  uint64_t total = query_machine_config_device();
+#else
+  uint64_t total = 0;
+#endif
   return total;
 }
-}
+
+uint64_t nda_query_total_host_memory() { return query_machine_config_host(); }
+
+}  // extern "C"
