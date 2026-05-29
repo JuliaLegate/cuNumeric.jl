780229702ccdc816129be3d6d6a2b533b7ed971f
krasow <krasow@u.northwestern.edu>
2026-05-29 11:29:33 -0500

    update versioninfo() output. Remove the storage of .githash

--- src/utilities/preference.jl
+++ src/utilities/preference.jl
@@ -140,5 +140,22 @@ function find_dependency_paths(::Type{CNPreferences.JLL})
     return results
 end
 
-find_dependency_paths(::Type{CNPreferences.Developer}) = Dict{String,String}()
-find_dependency_paths(::Type{CNPreferences.Conda}) = Dict{String,String}()
+function find_dependency_paths(::Type{CNPreferences.Developer})
+    results = Dict{String,String}()
+    paths_to_search = [CUPYNUMERIC_LIBDIR]
+    if isdefined(@__MODULE__, :cupynumeric_jll)
+        append!(paths_to_search, getfield(@__MODULE__, :cupynumeric_jll).LIBPATH_list)
+    end
+    for (name, lib) in DEPS_MAP
+        results[name] = dirname(Libdl.find_library(lib, paths_to_search))
+    end
+    return results
+end
+
+function find_dependency_paths(::Type{CNPreferences.Conda})
+    results = Dict{String,String}()
+    for (name, lib) in DEPS_MAP
+        results[name] = dirname(Libdl.find_library(lib, [CUPYNUMERIC_LIBDIR]))
+    end
+    return results
+end
