0c381c2909a4f5e564e555d5096317ba27bbff6b
Nader-Rahhal <rahhalnader04@gmail.com>
2026-06-04 13:40:04 -0400

    forgot add the type lol

--- lib/cunumeric_jl_wrapper/src/types.cpp
+++ lib/cunumeric_jl_wrapper/src/types.cpp
@@ -166,4 +166,5 @@ void wrap_binary_ops(jlcxx::Module& mod) {
 void wrap_linalg_ops(jlcxx::Module& mod) {
   mod.set_const("SOLVE", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_SOLVE});
   mod.set_const("MP_SOLVE", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_MP_SOLVE});
+  mod.set_const("SVD", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_SVD});
 }
