022b774fce76b13b6d0b1afb7134d090d6114068
Nader-Rahhal <rahhalnader04@gmail.com>
2026-06-04 13:48:36 -0400

    add types for other linalg ops

--- lib/cunumeric_jl_wrapper/src/types.cpp
+++ lib/cunumeric_jl_wrapper/src/types.cpp
@@ -167,4 +167,7 @@ void wrap_linalg_ops(jlcxx::Module& mod) {
   mod.set_const("SOLVE", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_SOLVE});
   mod.set_const("MP_SOLVE", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_MP_SOLVE});
   mod.set_const("SVD", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_SVD});
+  mod.set_const("QR", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_QR});
+  mod.set_const("SYEV", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_SYEV});
+  mod.set_const("GEEV", legate::LocalTaskID{CuPyNumericOpCode::CUPYNUMERIC_GEEV});
 }
