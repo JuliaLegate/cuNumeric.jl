7a1752911ba5596a1719293ed6e74633d3c9cf69
krasow <krasow@u.northwestern.edu>
2026-08-18 03:51:29 -0500

    fix tests from segfaulting

--- test/distributed/test_p2p.jl
+++ test/distributed/test_p2p.jl
@@ -1,29 +1,10 @@
-ENV["LEGATE_CONFIG"] = "--cpus=2 --show-config --logging legate=debug,level=2 --show-progress  --show-memory-usage  --profile"
+using Test
 
-using Distributed
-using cuNumeric
-
-cuNumeric.Experimental(false)
-try
-    cuNumeric.addprocs(1)
-    error("cuNumeric.addprocs should require Experimental mode")
-catch err
-    err isa ArgumentError || rethrow()
-end
-
-cuNumeric.Experimental(true)
-cuNumeric.addprocs(1)
-
-# Test on workers - cuNumeric will load with p2p networking
-@everywhere workers() begin
-    println("Worker $(myid()): Testing cuNumeric")
-    println("Number of runtimes: ", cuNumeric.get_number_of_runtimes())
-    println("P2P plugin: ", get(ENV, "REALM_UCP_BOOTSTRAP_PLUGIN", "not set"))
-    println("Self info: ", get(ENV, "WORKER_SELF_INFO", "not set"))
-
-    a = cuNumeric.rand(100)
-    b = cuNumeric.rand(100)
-    c = -(a + b)
-
-    println("Worker $(myid()): Test complete, result shape: ", size(c))
+if get(ENV, "CUNUMERIC_PARALLEL_TEST_RUNNER", "0") == "1"
+    project = Base.active_project()
+    script = joinpath(@__DIR__, "p2p_worker.jl.inc")
+    run(`$(Base.julia_cmd()) --startup-file=no --project=$project $script`)
+    @test true
+else
+    include(joinpath(@__DIR__, "p2p_worker.jl.inc"))
 end
